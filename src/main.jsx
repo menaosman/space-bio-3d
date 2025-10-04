b7e4c9ffb3a8542d0bee25c2fc9825b0786792c9
Mena Mohamed Osman <104127946+menaosman@users.noreply.github.com>
2025-10-04 03:13:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./index.css";
 import SpaceBiologyLanding from "./SpaceBiologyLanding.jsx";
 import Adventure from "./pages/Adventure.jsx";
 
