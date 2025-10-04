de54e2f0b86bb715e04dbc0dba4cea7ea3287675
Mena Mohamed Osman <104127946+menaosman@users.noreply.github.com>
2025-10-05 00:32:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import SpaceBiologyLanding from "./SpaceBiologyLanding.jsx";
 import Dashboard from "./Dashboard.jsx";
 import AdventureHub from "./AdventureHub.jsx";
 import ExobotanyStory from "./ExobotanyStory.jsx";
+import ExobotanyStory from "./MicrobiologyStory.jsx";
 const router = createHashRouter([
   { path: "/", element: <SpaceBiologyLanding /> },
   { path: "/dashboard", element: <Dashboard /> },
   { path: "/adventure", element: <AdventureHub /> },
-   { path: "/adventure/exobotany", element: <ExobotanyStory /> },
+  { path: "/adventure/exobotany", element: <ExobotanyStory /> },
+  { path: "/adventure/exobotany", element: <MicrobiologyStory /> },
+  
 ]);
 
 export default function App() {
