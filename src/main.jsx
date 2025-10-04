03b78aa550ea4ca13baed58b295f18ef0c7579c5
Mena Mohamed Osman <104127946+menaosman@users.noreply.github.com>
2025-10-04 03:52:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
-
-import SpaceBiologyLanding from "./SpaceBiologyLanding.jsx";
-import Adventure from "./pages/Adventure.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SpaceBiologyLanding />} />
-        <Route path="/adventure" element={<Adventure />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
